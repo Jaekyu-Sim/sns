20c3e3a686d16aa305d40233b1c15c49ca9a6655
Jaekyu Sim <worb1605@naver.com>
2023-01-03 22:42:03 +0900

    23/01/03 소스코드 작성 및 commit

---
+++ src/main/java/com/example/sns/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.sns.controller;
+
+import com.example.sns.controller.request.UserJoinRequest;
+import com.example.sns.controller.response.Response;
+import com.example.sns.controller.response.UserJoinResponse;
+import com.example.sns.model.User;
+import com.example.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    // TODO : implements
+    @PostMapping("/join")
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
+        User user = userService.join(request.getUserName(), request.getPassword());
+        UserJoinResponse response = UserJoinResponse.fromUser(user);
+
+        return Response.success(response);
+    }
+}
---
+++ src/main/java/com/example/sns/service/UserService.java
@@ -0,0 +1,46 @@
+package com.example.sns.service;
+
+import com.example.sns.exception.ErrorCode;
+import com.example.sns.exception.SnsApplicationException;
+import com.example.sns.model.User;
+import com.example.sns.model.entity.UserEntity;
+import com.example.sns.repository.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserEntityRepository userEntityRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    // TODO : implement
+    @Transactional
+    public User join(String userName, String password) {
+        // 회원가입하려는 userName 으로 회원가입된 user가 있는지
+        userEntityRepository.findByUserName(userName).ifPresent(it -> {
+            throw  new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated", userName));
+        });
+        // 회원가입 진행 = user를 등록
+        UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, encoder.encode(password)));
+
+        return User.fromEntity(userEntity);
+    }
+
+    // TODO : implement
+    public String login(String userName, String password) {
+        // 회원가입 여부 체크
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, ""));
+        // 비밀번호 체크
+        if(!userEntity.getPassword().equals(password)){
+            throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, "");
+        }
+        // 토큰 생성
+        return "";
+    }
+}
